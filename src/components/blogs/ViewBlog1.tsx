4cf4a6c7b67f862ccb3421d3a86e01c7a426f851
bayrem98 <bayremsayeh98@gmail.com>
2023-12-06 12:39:29 +0100

    convert css value from blog1 file to appcss file

--- src/components/blogs/ViewBlog1.tsx
+++ src/components/blogs/ViewBlog1.tsx
@@ -8,33 +8,20 @@ const ViewBlog1 = () => {
 
   return (
     <>
-      <div className="">
+      <div>
         <FontAwesomeIcon
+          className="blog1-icon-retour"
           icon={faCircleLeft}
           beat
           size="2xl"
-          style={{
-            color: "rgba(147, 147, 203, 0.866",
-            cursor: "pointer",
-            marginTop: 10,
-            marginLeft: 10,
-          }}
           onClick={() => navigate(-1)}
         />
-
-        <Card
-          style={{
-            marginTop: 20,
-            marginLeft: 50,
-            marginRight: 50,
-            marginBottom: 20,
-          }}
-        >
-          <h2 style={{ textAlign: "center" }}>
+        <Card className="blog1-card">
+          <h2 className="blog1-title">
             Quelle couleur est associée à votre signe astrologique ?
           </h2>
           <CardBody>
-            <p style={{ textAlign: "justify" }}>
+            <p className="blog1-para">
               Avant d’énumérer les couleurs associées à chaque signe
               astrologique, faisons un tour d’horizon sur les signes en
               question. L’astrologie divise le zodiaque en douze signes
@@ -43,41 +30,51 @@ const ViewBlog1 = () => {
               le rendent singulier. Ces signes sont regroupés en quatre éléments
               fondamentaux : Feu, Terre, Air et Eau, chacun apportant son propre
               ensemble de qualités.
-              <ul>
-                <li>
+            </p>
+            <ul className="blog1-ul">
+              <li className="blog1-li">
+                <span className="blog1-span">
                   Les signes de Feu (Bélier, Lion, Sagittaire) sont connus pour
                   leur énergie, leur passion et leur dynamisme. Ils sont souvent
                   associés à des couleurs chaudes et vives comme le rouge et le
                   jaune, symbolisant leur esprit ardant.
-                </li>
-                <li>
+                </span>
+              </li>
+              <li className="blog1-li">
+                <span className="blog1-span">
                   Les signes de Terre (Taureau, Vierge, Capricorne) sont
                   pragmatiques, stables et axés sur la matérialité. Les couleurs
                   associées sont le vert et le brun, reflétant leur connexion à
                   la nature et à la croissance.
-                </li>
-                <li>
+                </span>
+              </li>
+              <li className="blog1-li">
+                <span className="blog1-span">
                   Les signes d’Air (Gémeaux, Balance, Verseau) sont
                   intellectuels, communicatifs et sociaux. Les teintes aériennes
                   comme le bleu clair et le pastel correspondent à leur nature
                   aérée.
-                </li>
-                <li>
+                </span>
+              </li>
+              <li className="blog1-li">
+                <span className="blog1-span">
                   Les signes d’Eau (Cancer, Scorpion, Poissons) sont
                   émotionnels, intuitifs et sensibles. Les couleurs associées
                   sont le bleu profond et le violet, évoquant leur profondeur
                   émotionnelle.
-                </li>
-              </ul>
+                </span>
+              </li>
+            </ul>
+            <p className="blog1-para">
               Chaque signe astrologique possède ses propres couleurs qui
               reflètent ses caractéristiques uniques, et ces correspondances
               peuvent être utilisées pour choisir des vêtements, des décors ou
               des nuances qui favorisent l’harmonie et le bien-être personnel.
             </p>
-            <h6>
+            <h6 className="blog1-title2-3">
               *Pourquoi déterminer les couleurs associées à votre signe astro ?
             </h6>
-            <p>
+            <p className="blog1-para">
               Psychologie des couleurs, chromothérapie, pouvoir de la
               colorimétrie… peu importe comment vous voulez l’appeler, toutes
               ces disciplines se réunissent autour d’une même idée : les
@@ -94,80 +91,108 @@ const ViewBlog1 = () => {
               qui vous font du bien et qui résonnent avec votre signe peut vous
               aider à atteindre votre plein potentiel.
             </p>
-            <h6>
+            <h6 className="blog1-title2-3">
               *Laquelle des douze couleurs est associée à votre signe
               astrologique ?
             </h6>
-            <p style={{ textAlign: "justify" }}>
-              <ul>
-                <li>
-                  Bélier Le Bélier est un signe de feu et est facilement attiré
-                  par les couleurs vives. Les personnes nées sous les signes du
-                  feu ont une personnalité distinctive et sont pleines
-                  d’énergie, caractérisées par l’enthousiasme et l’insouciance.
+            <br />
+            <div>
+              <ul className="blog1-ul">
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Bélier Le Bélier est un signe de feu et est facilement
+                    attiré par les couleurs vives. Les personnes nées sous les
+                    signes du feu ont une personnalité distinctive et sont
+                    pleines d’énergie, caractérisées par l’enthousiasme et
+                    l’insouciance.
+                  </span>
                 </li>
-                <li>
-                  Taureau Symbole d’amour pour la nature et les grands espaces,
-                  il fallait lui consacrer le vert. Pour la richesse végétale et
-                  le caractère terreux, c’est la couleur la mieux adaptée à ce
-                  signe de terre.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Taureau Symbole d’amour pour la nature et les grands
+                    espaces, il fallait lui consacrer le vert. Pour la richesse
+                    végétale et le caractère terreux, c’est la couleur la mieux
+                    adaptée à ce signe de terre.
+                  </span>
                 </li>
-                <li>
-                  Gémeaux Les Gémeaux n’aiment généralement pas le noir ou le
-                  blanc, mais aiment un heureux mélange des deux ! Les Gémeaux
-                  sont le signe de la dualité et des moitiés, préférant le gris.
-                  Les signes aériens les associent implicitement au gris, comme
-                  le ciel et les nuages.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Gémeaux Les Gémeaux n’aiment généralement pas le noir ou le
+                    blanc, mais aiment un heureux mélange des deux ! Les Gémeaux
+                    sont le signe de la dualité et des moitiés, préférant le
+                    gris. Les signes aériens les associent implicitement au
+                    gris, comme le ciel et les nuages.
+                  </span>
                 </li>
-                <li>
-                  Cancer Les natifs de ce signe sont intelligents et émotifs. Ce
-                  signe très doux est associé à la couleur blanche. La pureté et
-                  le calme sont véritablement associés à cette couleur.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Cancer Les natifs de ce signe sont intelligents et émotifs.
+                    Ce signe très doux est associé à la couleur blanche. La
+                    pureté et le calme sont véritablement associés à cette
+                    couleur.
+                  </span>
                 </li>
-                <li>
-                  Lion Puisque le lion est le roi de la jungle et possède une
-                  belle fourrure jaune, cette couleur lui va à merveille.
-                  Faisant référence au soleil qui fait briller le lion, la
-                  couleur jaune s’accorde parfaitement avec ce signe qui adore
-                  rouler des yeux.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Lion Puisque le lion est le roi de la jungle et possède une
+                    belle fourrure jaune, cette couleur lui va à merveille.
+                    Faisant référence au soleil qui fait briller le lion, la
+                    couleur jaune s’accorde parfaitement avec ce signe qui adore
+                    rouler des yeux.
+                  </span>
                 </li>
-                <li>
-                  Vierge Personnalité rêveuse, sensuelle et intuitive : voilà ce
-                  qui décrit les personnes Vierge. En raison de l’espoir et de
-                  la tendresse qu’ils représentent, le vert est la couleur
-                  parfaite pour ce signe de terre.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Vierge Personnalité rêveuse, sensuelle et intuitive : voilà
+                    ce qui décrit les personnes Vierge. En raison de l’espoir et
+                    de la tendresse qu’ils représentent, le vert est la couleur
+                    parfaite pour ce signe de terre.
+                  </span>
                 </li>
-                <li>
-                  Balance Les Balances sont des personnes équilibrées et
-                  préfèrent les couleurs équilibrées entre deux tons. Nous avons
-                  choisi le saumon, car il n’était ni orange ni rose, c’était la
-                  combinaison parfaite des deux !
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Balance Les Balances sont des personnes équilibrées et
+                    préfèrent les couleurs équilibrées entre deux tons. Nous
+                    avons choisi le saumon, car il n’était ni orange ni rose,
+                    c’était la combinaison parfaite des deux !
+                  </span>
                 </li>
-                <li>
-                  Scorpion Quoi de mieux que du noir pour ce mystérieux signe
-                  astro !
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Scorpion Quoi de mieux que du noir pour ce mystérieux signe
+                    astro !
+                  </span>
                 </li>
-                <li>
-                  Sagittaire Saviez-vous que la couleur violette est associée à
-                  la spiritualité ? Bien que surprenant, le violet est plus
-                  facile à assortir à de nombreux styles que vous ne le pensez.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Sagittaire Saviez-vous que la couleur violette est associée
+                    à la spiritualité ? Bien que surprenant, le violet est plus
+                    facile à assortir à de nombreux styles que vous ne le
+                    pensez.
+                  </span>
                 </li>
-                <li>
-                  Capricorne En tant que signe de terre, on associe la couleur
-                  terre cuite aux troncs d’arbres, à la capacité rassurante du
-                  Capricorne et à un pilier de force parmi ses pairs.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Capricorne En tant que signe de terre, on associe la couleur
+                    terre cuite aux troncs d’arbres, à la capacité rassurante du
+                    Capricorne et à un pilier de force parmi ses pairs.
+                  </span>
                 </li>
-                <li>
-                  Verseau Avec une imagination sans limites et la tête dans le
-                  ciel et les nuages, le bleu foncé est la couleur parfaite pour
-                  ce signe maussade.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Verseau Avec une imagination sans limites et la tête dans le
+                    ciel et les nuages, le bleu foncé est la couleur parfaite
+                    pour ce signe maussade.
+                  </span>
                 </li>
-                <li>
-                  Poisson C’est le signe d’eau par excellence, il faut donc
-                  l’associer au bleu clair et à la mer.
+                <li className="blog1-li">
+                  <span className="blog1-span">
+                    Poisson C’est le signe d’eau par excellence, il faut donc
+                    l’associer au bleu clair et à la mer.
+                  </span>
                 </li>
               </ul>
-            </p>
+            </div>
           </CardBody>
         </Card>
       </div>
